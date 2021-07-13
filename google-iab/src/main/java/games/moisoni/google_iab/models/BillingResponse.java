7965e02c2e4a65cc63435b24646ca495e7d42e45
Moisoni Ioan <moisoniioan@gmail.com>
2021-07-13 21:28:24 +0300

    Added toString() method

--- google-iab/src/main/java/games/moisoni/google_iab/models/BillingResponse.java
+++ google-iab/src/main/java/games/moisoni/google_iab/models/BillingResponse.java
@@ -1,5 +1,7 @@
 package games.moisoni.google_iab.models;
 
+import androidx.annotation.NonNull;
+
 import com.android.billingclient.api.BillingResult;
 
 import games.moisoni.google_iab.enums.ErrorType;
@@ -31,5 +33,12 @@ public class BillingResponse {
 
     public int getResponseCode() {
         return responseCode;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "BillingResponse: Error type: " + errorType +
+                " Response code: " + responseCode + " Message: " + debugMessage;
     }
 }
